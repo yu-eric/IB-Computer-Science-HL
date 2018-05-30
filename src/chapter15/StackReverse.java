79589fcdc24dfefcfc15b0ff29686ac98d3c855a
Eric <massivefish@users.noreply.github.com>
2018-05-30 09:21:46 -0600

    Added StackReverse

--- src/chapter15/StackReverse.java
+++ src/chapter15/StackReverse.java
@@ -11,22 +11,45 @@ public class StackReverse {
 
     public static void main(String[] args) {
 
-        Stack<String> sentence = new Stack<>();
 
         Scanner in = new Scanner(System.in);
-        System.out.println("Please enter a sentence to reverse, ending with a period, question mark, or exclamation mark: ");
-        boolean noPunctuation = true;
+        System.out.println("Please enter a sentence to reverse, ending with a period: ");
+        String next = in.nextLine();
+        String[] sentence = next.split("\\.");
+        Stack<String> words = new Stack<>();
+        String[] wordList = new String[] {};
+        boolean space = true;
 
-        while(noPunctuation) {
-            String word = in.next();
-            sentence.push(word);
-            if(word.contains(".") || word.contains("?") || word.contains("!")) {
-                noPunctuation = false;
+        for(int i = 0; i < sentence.length; i++) {
+            wordList = sentence[i].split(" ");
+
+            wordList[i] = wordList[i].toLowerCase();
+            wordList[0] += ". ";
+            if(i != 0) {
+                wordList[wordList.length - 1] += " ";
             }
+            for(int j = 0; j < wordList.length; j++) {
+                int theIndex = wordList.length - 1;
+                if(j == theIndex) {
+
+                    wordList[theIndex] = wordList[theIndex].replace(wordList[theIndex].charAt(0), wordList[theIndex].toUpperCase().charAt(0));
+                }
+                words.push(wordList[j]);
+
+                while(space) {
+                    words.push(" ");
+                    space = false;
+                }
+            }
+
         }
 
-        while(!sentence.isEmpty()) {
-            System.out.print(sentence.pop() + " ");
+
+
+
+
+        while(!words.isEmpty()) {
+            System.out.print(words.pop());
         }
     }
 }
